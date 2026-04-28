6af9b926856af51eccfb98ac12ea81502a342e17
RoniLopes <ronisleylopes@yahoo.com.br>
2026-04-28 22:43:33 +0000

    Converti os valores do PIB das cartas para o número real em bilhões.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,11 +10,11 @@ int main(){
     char cidade1[20] = "São Paulo";
     int populacao1 = 12325000;
     float area1 = 1521.11;
-    float pib1 = 699.28;
+    float pib1 = 699280000000;
     int pontos1 = 50;
     float densidade_pop1 = populacao1 / area1;
-    
-
+    float pib_per_cap1 = pib1 / populacao1;
+ 
     printf("Carta 1: \n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -23,16 +23,19 @@ int main(){
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Números de Pontos Turísticos: %d\n", pontos1);
-    printf("Densidade Populacional: %.2f\n\n", densidade_pop1);
+    printf("Densidade Populacional: %.2f\n", densidade_pop1);
+    printf("PIB per Capita: %.2f\n\n", pib_per_cap1);
 
     char estado2 = 'B';
     char codigo2[10] = "B02";
     char cidade2[20] = "Rio de Janeiro";
     int populacao2 = 6748000;
     float area2 = 1200.25;
-    float pib2 = 300.50;
+    float pib2 = 300500000000;
     int pontos2 = 30;
-
+    float densidade_pop2 = populacao2 / area2;
+    float pib_per_cap2 = pib2 / populacao2;
+    
     printf("Carta 2: \n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
@@ -40,7 +43,9 @@ int main(){
     printf("População: %d\n", populacao2);
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Números de Pontos Turísticos: %d\n\n", pontos2);
+    printf("Números de Pontos Turísticos: %d\n", pontos2);
+    printf("Densidade Populacional: %.2f\n", densidade_pop2);
+    printf("PIB per Capita: %.2f\n\n", pib_per_cap2);
 
 return 0;
 
